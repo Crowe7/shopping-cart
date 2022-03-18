0220f152a49d5bf1cb51d9bbd832d28f052e8734
Crowe7 <jacobocrowe@gmail.com>
2022-03-18 05:20:27 -0500

    added component folder and todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
 
+// Routes for home and the shopping cart. THESE GO IN HERE!
+//navbar that lets people go to home and cart MAKE SURE TO LOAD NAVBAR COMPONENT IN EACH ROUTE
+//navbar needs state to display how many items are in cart
+// array that contains each item to be mapped into the main page / shopping cart
+//checkout needs to show whats in cart and also allow changes to quantity in popup
+
+// product items should be objects with the following info... Title price quantity
+// product cards need plus and minus buttons along with being able to type in quantity number
+// product cards need a go to cart button
+
+// responsive design on this one along with using grid
+// tesing as well
+//check framer motion
+
+// TODO COMPONENTS:
+// APP : STATE IS WHATS IN CART
+// NAVBAR : GET NUMBER OF ITEMS IN CART BY USEEFFECT HOOK CHANGING WHEN CART STATE CHANGES. CHECKING ARRAY.LENGTH PASSED BY APP
+// HOMEPAGE : PULLS STATE FROM APP
+// CHECKOUT GET NUMBER OF ITEMS IN CART AND ALSO HAS ADD TO CART AND REMOVE FUNCTIONS... ALONG WITH TOTAL PRICE 
+// CARD : PULLS ADD TO CART FUNCTION FROM APP
+
+
+
+
 function App() {
   return (
     <div>shopcart</div>
