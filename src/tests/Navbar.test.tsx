9ba3abf931f415c179a06e9698ecd26cf3ab998c
Crowe7 <jacobocrowe@gmail.com>
2022-03-22 02:34:13 -0500

    fixed tests not running

--- src/tests/Navbar.test.tsx
+++ src/tests/Navbar.test.tsx
@@ -1,8 +1,16 @@
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+   ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+})); // had to use this to get tests to run 
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import {Navbar} from '../components/Navbar';
+import {Routes} from 'react-router-dom'
 // aria-label='edit'> add that onto label buttons to test
 describe('Navbar', () => {
+
     test("Has checkout page button", () => {
         render(<Navbar/>);
          expect(screen.getByRole("button", {name:"Checkout"})).toBeInTheDocument()
