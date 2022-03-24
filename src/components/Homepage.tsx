32c533f7360f910df6af88665f935530c192ef5d
Crowe7 <jacobocrowe@gmail.com>
2022-03-24 06:22:40 -0500

    made mobile responsive

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -7,21 +7,21 @@ import {Link} from 'react-router-dom'
 export default function Homepage() {
   return (
     <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", height: "80vh"}}>
-      <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", height: "1000px" }}>
+      <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", '@media (max-width: 450px)': {flexDirection: "column-reverse"}}}>
         <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", height: 150}}>
-          <Title sx={{fontSize: 40, padding: 20}} order={1}>Unlo<span style={{color: "#F1FAEE"}}>cking Fun for the Whole Fam</span>ily! </Title>
+          <Title sx={{fontSize: 40, padding: 20, '@media (max-width: 450px)': {color: "#F1FAEE"}}} order={1}>Unlo<span style={{color: "#F1FAEE",}}>cking Fun for the Whole Fam</span>ily! </Title>
           <Button sx={{width: "50%", padding: 20}} size='xl' component={Link} to='/products'>View Products</Button>
           <Image
             src={homepageBlob}
             width={900}
-            sx={{position: "relative", zIndex: -1, bottom: 350}}
+            sx={{position: "relative", zIndex: -1, bottom: 350, '@media (max-width: 450px)': {display: "none"}}}
           />
         </Box>
         <Image
+          sx={{width: 630, height: 330, '@media (max-width: 450px)': {height:"200px", width: "300px"}}}
           radius="lg"
           src={homepageImage}
-          width={600}
-          height={330}
+
           alt="Group Of Creative Friends Sitting At Wooden Table. People Having Fun While Playing Board Game"
       />
       </Box>
