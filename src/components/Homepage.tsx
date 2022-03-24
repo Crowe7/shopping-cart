a799d64426e1b5891a778cbd33b73815ea290fbd
Crowe7 <jacobocrowe@gmail.com>
2022-03-24 06:03:03 -0500

    added non responsive styles to homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import background from '../media/background.svg'
-import a from '../media/Azul.png'
-import { Box } from '@mantine/core'
+import homepageImage from '../media/homepageImage.jpg'
+import homepageBlob from '../media/homepageBlob.png'
+import { Box, Image, Title, Button, } from '@mantine/core'
+import {Link} from 'react-router-dom'
 export default function Homepage() {
   return (
-    <>
-      <h1>HOMEPAGE</h1>
+    <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", height: "80vh"}}>
+      <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", height: "1000px" }}>
+        <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", height: 150}}>
+          <Title sx={{fontSize: 40, padding: 20}} order={1}>Unlo<span style={{color: "#F1FAEE"}}>cking Fun for the Whole Fam</span>ily! </Title>
+          <Button sx={{width: "50%", padding: 20}} size='xl' component={Link} to='/products'>View Products</Button>
+          <Image
+            src={homepageBlob}
+            width={900}
+            sx={{position: "relative", zIndex: -1, bottom: 350}}
+          />
+        </Box>
+        <Image
+          radius="lg"
+          src={homepageImage}
+          width={600}
+          height={330}
+          alt="Group Of Creative Friends Sitting At Wooden Table. People Having Fun While Playing Board Game"
+      />
+      </Box>
       <Box sx={{
               position: "absolute",
               bottom: 0,
@@ -15,8 +34,9 @@ export default function Homepage() {
               backgroundPosition: "center",
               backgroundSize: "cover",
               backgroundImage: `url(${background})`,
+              zIndex: -2,
               '@media (max-width: 450px)': {aspectRatio: "960/2500",}
             }}></Box>
-    </>
+    </Box>
   )
 }
