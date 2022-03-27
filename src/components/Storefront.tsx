b360ea2e1ef67b9ed72f13635a0fbbeee3039853
Crowe7 <jacobocrowe@gmail.com>
2022-03-27 04:22:10 -0500

    made storefront responsive

--- src/components/Storefront.tsx
+++ src/components/Storefront.tsx
@@ -5,16 +5,26 @@ import { ProductData } from '../utils/productData'
 
 export const Storefront = () => {
   return (
-    <SimpleGrid cols={4} spacing="xl">
+    <SimpleGrid 
+      cols={4} 
+      spacing="xl" 
+      sx={{padding: "100px", '@media (max-width: 880px)': {padding: "50px"}}}
+      breakpoints={[
+        {maxWidth: 1555, cols: 3, spacing: "lg"},
+        {maxWidth: 1150, cols: 2, spacing: "md"},
+        {maxWidth: 880, cols: 1, spacing: "md"},
+
+      ]}
+    >
       {ProductData.map((info) => {
-          return <Card shadow="sm" key={`${info.ID}`}>
+          return <Card sx={{backgroundColor: "#EAF5ED",  }} shadow="sm" key={`${info.ID}`}>
             <Card.Section>
-              <Image src={info.Img} height={450} alt={info.Name} />
+              <Image src={info.Img} height={450} alt={info.Name} fit={"contain"} />
             </Card.Section>
             <Box>
-              <Text weight={1000}>{`${info.Name}`}</Text>
-              <Code color="#E63946">{ "$" + info.Price}</Code>
-              <Button fullWidth component={Link} to={`/products/${info.Name}`}>{`${info.Name}`}</Button>
+              <Text sx={{textAlign: "center", fontSize: "24px"}} weight={700}>{`${info.Name}`}</Text>
+              <Code color="pink" sx={{fontSize: 16}}>{ "$" + info.Price}</Code>
+              <Button sx={{marginTop: 10}} fullWidth component={Link} to={`/products/${info.Name}`}>View Product</Button>
             </Box>
           </Card>
         })
