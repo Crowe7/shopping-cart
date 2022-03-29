50717735e4153cd82db22b91013184c4977b20bc
Crowe7 <jacobocrowe@gmail.com>
2022-03-29 03:40:25 -0500

    added product page/ error page if args are invalid

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import {getProduct} from '../utils/data'
+import { productInterface } from '../utils/productData'
+// function(arg: type): returnType how to type out function props
 
-export default function Product() {
+type MyProps = {
+  addToCart(name: string) : void
+}
+
+export const Product = ({addToCart}: MyProps) => {
+  let params = useParams();
+  let productInfo: null | productInterface = null
+  
+  try {
+    productInfo = getProduct(params.productID)
+  } catch(error) {
+    productInfo = null
+  }
+
+  if(productInfo) {  
+    return (
+      <div>{productInfo.Name}</div>
+    )
+  }
   return (
-    <div>Product</div>
+    <div>This will be an error page i suppose</div>
   )
 }
+
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
-import {MemoryRouter as MRouter} from 'react-router-dom'
+import {MemoryRouter as MRouter} from 'react-router-dom';
 describe("App", () => {
     test("contains navbar", () => {
         render(
