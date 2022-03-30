6bf0177f70aa474c20895bfc60bf21268ecec309
Crowe7 <jacobocrowe@gmail.com>
2022-03-30 06:33:30 -0500

    made responsive

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Image, SimpleGrid, Title } from '@mantine/core'
+import { BackgroundImage, Box, Button, Image, MediaQuery, SimpleGrid, Title } from '@mantine/core'
+import { height } from '@mui/system'
 import React, { MouseEventHandler } from 'react'
 import { useParams } from 'react-router-dom'
 import {getProduct} from '../utils/data'
@@ -22,9 +23,19 @@ export const Product = ({addToCart}: MyProps) => {
 
   if(productInfo) {  
     return (
-      <Box sx={{display: "flex", justifyContent: "center", height: "100vh", alignItems: "center", '@media (max-width: 880px)': {flexDirection: "column"}}}>
-        <Image sx={{backgroundColor: "#eef7fa"}} src={productInfo.Img} height={550} width={550} fit={"contain"} />
-        <SimpleGrid cols={1} spacing="sm" sx={{padding: 100}}>
+      <Box sx={{display: "flex", justifyContent: "center", height: "90vh", alignItems: "center", '@media (max-width: 880px)': {flexDirection: "column", marginTop: 100}}}>
+        <Box sx={{
+                  backgroundImage: `url(${productInfo.Img})`,
+                  backgroundSize: "contain",
+                  backgroundColor: "#eef7fa",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  width: 550, 
+                  height: 550, 
+                  '@media (max-width: 880px)': {width: 350} 
+                }}>
+        </Box>
+        <SimpleGrid cols={1} spacing="sm" sx={{padding: 100, '@media (max-width: 880px)': {display: "flex", justifyContent: "center", flexDirection: "column", alignItems:"center"}}}>
           <Title order={1}>{`${productInfo.Name}`}</Title>
           <Title order={2}>{` Price: $${productInfo.Price}`}</Title>
           <Button size='xl' onClick={addToCart(`${productInfo?.Name}`)}>Add To Cart</Button>
