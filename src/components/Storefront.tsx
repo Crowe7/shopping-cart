865caf2f8949c60da91948e121285ef3626b64cc
Crowe7 <jacobocrowe@gmail.com>
2022-05-16 14:45:53 -0500

    refactored Storefront component styles

--- src/components/Storefront.tsx
+++ src/components/Storefront.tsx
@@ -1,14 +1,42 @@
-import { Box, Button, Card, Code, Image, SimpleGrid, Text } from '@mantine/core'
+import { Box, Button, Card, Code, createStyles, Image, SimpleGrid, Text } from '@mantine/core'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { ProductData } from '../utils/productData'
 
+
+const useStyles = createStyles((theme) => ({
+  gridWrapper: {
+    padding: "100px", 
+    '@media (max-width: 880px)': {padding: "50px"}
+  },
+
+  productCard: {
+    backgroundColor: "#eef7fa"
+  },
+
+  productName: {
+    textAlign: "center", 
+    fontSize: "24px"
+  },
+
+  productPrice: {
+    fontSize: 16
+  },
+
+  viewProductButton: {
+    marginTop: 10
+  }
+
+}))
+
+
 export const Storefront = () => {
+  const { classes } = useStyles()
   return (
     <SimpleGrid 
       cols={4} 
       spacing="xl" 
-      sx={{padding: "100px", '@media (max-width: 880px)': {padding: "50px"}}}
+      className={classes.gridWrapper}
       breakpoints={[
         {maxWidth: 1555, cols: 3, spacing: "lg"},
         {maxWidth: 1150, cols: 2, spacing: "md"},
@@ -17,14 +45,14 @@ export const Storefront = () => {
       ]}
     >
       {ProductData.map((info) => {
-          return <Card sx={{backgroundColor: "#eef7fa",  }} shadow="sm" key={`${info.ID}`}>
+          return <Card className={classes.productCard} shadow="sm" key={`${info.ID}`}>
             <Card.Section>
               <Image src={info.Img} height={450} alt={info.Name} fit={"contain"} />
             </Card.Section>
             <Box>
-              <Text sx={{textAlign: "center", fontSize: "24px"}} weight={700}>{`${info.Name}`}</Text>
-              <Code color="pink" sx={{fontSize: 16}}>{ "$" + info.Price}</Code>
-              <Button sx={{marginTop: 10}} fullWidth component={Link} to={`/products/${info.Name}`}>View Product</Button>
+              <Text className={classes.productName} weight={700}>{`${info.Name}`}</Text>
+              <Code color="pink" className={classes.productPrice}>{ "$" + info.Price}</Code>
+              <Button className={classes.viewProductButton} fullWidth component={Link} to={`/products/${info.Name}`}>View Product</Button>
             </Box>
           </Card>
         })
