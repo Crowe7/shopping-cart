31a79ee4d8007987cb0a624a104116302c0e5bf6
Crowe7 <jacobocrowe@gmail.com>
2022-05-15 22:48:47 -0500

    Refactor styles on checkout component

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, ScrollArea, SimpleGrid, Title } from '@mantine/core'
+import { Box, Button, createStyles, ScrollArea, SimpleGrid, Title } from '@mantine/core'
 import React, { MouseEventHandler } from 'react'
 import { productInterface } from '../utils/productData'
 import { Link } from 'react-router-dom'
@@ -14,46 +14,125 @@ type MyProps = {
   itemValue(name:string): void,
   removeAllSameItems(name:string): MouseEventHandler<HTMLButtonElement>,
 }
+const useStyles = createStyles((theme) => ({
+  emptyCartWrapper: {
+    width: "100vw", 
+    height: "90vh", 
+    display: "flex", 
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: "50px"
+  },
+  emptyCartButton: {
+    marginTop: 20
+  },
 
+
+  cartWrapper: {
+    display: "flex", 
+    justifyContent: "center",
+    alignItems: "center",
+    '@media (max-width: 1380px)': {flexDirection: 'column-reverse'}
+  },
+
+  scrollArea: {
+    height: "90vh",
+    '@media (max-width: 1380px)': {height: "80vh"},
+    '@media (max-width: 980px)': {height: "70vh"}
+  },
+
+  gridWrapper: {
+    width: "700px",
+    marginTop: "6vh",
+    marginLeft: "100px",
+    '@media (max-width: 980px)': {marginLeft: 0}
+  },
+
+  productBoxWrapper: {
+    height:"200px"
+  },
+
+  productTitle: {
+    display: "flex",
+    justifyContent: "center"
+  },
+
+  productWrapper: {
+    display: "flex",
+    justifyContent: "center"
+  },
+
+  addRemoveWrapper: {
+    display:"flex",  
+    width: 100, 
+    justifyContent: "space-around"
+  },
+
+  addRemoveButton: {
+    fontSize: "16px"
+  },
+
+  imgWrapper: {
+    marginLeft: "275px",
+    '@media (max-width: 1380px)': {marginLeft: 0}
+  },
+
+  img: {
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    width: 100, 
+    height: 100, 
+  },
+
+  buttonWrapper: {
+    display: "flex",  
+    flexDirection: "column", 
+    '@media (min-width: 1000px)': {marginLeft:"100px"}, 
+    alignItems:"center", 
+    marginBottom: "10px"
+  },
+
+  clearAndCheckoutWrapper: {
+    display: "flex", 
+    gap: "15px"
+  },
+
+  clearCart: {
+    marginLeft:"70px"
+  },
+
+
+}))
 
 export const Checkout = ({cart, addToCart, removeFromCart, clearCart, value, itemValue, removeAllSameItems}: MyProps) => {
+  const { classes } = useStyles();
   if(cart.length > 0) {
     //remove all duplicates from an array of objects with filter.. use a map for large data sets!
     let uniqueProducts = cart.filter((product, idx, self) => 
       self.findIndex(p => p.Name === product.Name) === idx);
 
     return (
-      <Box sx={{display: "flex", 
-                justifyContent: "center",
-                alignItems: "center",
-                '@media (max-width: 1380px)': {flexDirection: 'column-reverse'}
-              }}>
-        <ScrollArea scrollbarSize={7} sx={{height: "90vh", '@media (max-width: 1380px)': {height: "80vh"}, '@media (max-width: 980px)': {height: "70vh"}}}>     
-        <SimpleGrid cols={1} spacing="xl" sx={{width: "700px", marginTop: "6vh", marginLeft: "100px", '@media (max-width: 980px)': {marginLeft: 0} }} >
+      <Box className={classes.cartWrapper}>
+        <ScrollArea scrollbarSize={7} className={classes.scrollArea}>     
+        <SimpleGrid cols={1} spacing="xl" className={classes.gridWrapper} >
           {uniqueProducts.map((product) => {
               let quantity = cart.filter(p => p.Name === product.Name).length
-              return  <Box key={product.Name} sx={{height:"200px"}}>
-                <Box sx={{display: "flex", justifyContent: "center"}}>
+              return  <Box key={product.Name} className={classes.productBoxWrapper}>
+                <Box className={classes.productTitle}>
                   <Title order={1}>{product.Name}</Title>
                 </Box>
-                <Box sx={{display: "flex", justifyContent: "center"}}>
-                  <Box sx={{marginLeft: "275px", '@media (max-width: 1380px)': {marginLeft: 0}}}>
-                    <Box sx={{
-                      backgroundImage: `url(${product.Img})`,
-                      backgroundSize: "cover",
-                      backgroundRepeat: "no-repeat",
-                      backgroundPosition: "center",
-                      width: 100, 
-                      height: 100, 
-                    }}>
-                    </Box>
-                    <Box sx={{display:"flex",  width: 100, justifyContent: "space-around"}}>
-                      <Button size='xs' aria-label='remove' onClick={removeFromCart(product.Name)} sx={{fontSize: "16px"}} variant="subtle"><FontAwesomeIcon icon={faAngleLeft}/></Button>
+                <Box className={classes.productWrapper}>
+                  <Box className={classes.imgWrapper}>
+                    <Box sx={{backgroundImage: `url(${product.Img})`}} className={classes.img}>
+                  </Box>
+                    <Box className={classes.addRemoveWrapper}>
+                      <Button size='xs' aria-label='remove' onClick={removeFromCart(product.Name)} className={classes.addRemoveButton} variant="subtle"><FontAwesomeIcon icon={faAngleLeft}/></Button>
                       <Title order={3}>{quantity}</Title>
-                      <Button size='xs' aria-label='add' onClick={addToCart(product.Name)} sx={{fontSize: "16px"}} variant="subtle"><FontAwesomeIcon icon={faAngleRight}/></Button>
+                      <Button size='xs' aria-label='add' onClick={addToCart(product.Name)} className={classes.addRemoveButton} variant="subtle"><FontAwesomeIcon icon={faAngleRight}/></Button>
                     </Box>
                   </Box>
-                  <Box sx={{marginLeft:"70px"}}>
+                  <Box className={classes.clearCart}>
                     <Title order={2}>{`Item Total: $${itemValue(product.Name)}`}</Title>
                     <Button size='lg' onClick={removeAllSameItems(product.Name)}>Remove From Cart</Button>
                   </Box>
@@ -63,9 +142,9 @@ export const Checkout = ({cart, addToCart, removeFromCart, clearCart, value, ite
           }
         </SimpleGrid>
         </ScrollArea>   
-        <Box sx={{display: "flex", flexDirection: "column", '@media (min-width: 1000px)': {marginLeft:"100px"}, alignItems:"center", marginBottom: "10px"}}>
+        <Box className={classes.buttonWrapper}>
           <Title order={1}>{`Cart Total: $${value}`}</Title>
-          <Box sx={{display: "flex", gap: "15px"}}>
+          <Box className={classes.clearAndCheckoutWrapper}>
             <Button size='xl' onClick={clearCart}>Clear Cart</Button>
             <Button size='xl' onClick={clearCart}>Checkout</Button>
           </Box>
@@ -74,9 +153,9 @@ export const Checkout = ({cart, addToCart, removeFromCart, clearCart, value, ite
     )
   }
   return (
-    <Box sx={{ width: "100vw", height: "90vh", display: "flex", flexDirection: "column", alignItems: "center", marginTop: "50px"}}>
+    <Box className={classes.emptyCartWrapper}>
       <Title order={1}>Your Cart Is Empty! </Title>
-      <Button sx={{marginTop: 20}} size='xl' component={Link} to='/products'>Shop Products</Button>
+      <Button className={classes.emptyCartButton} size='xl' component={Link} to='/products'>Shop Products</Button>
     </Box>
   )
 }
