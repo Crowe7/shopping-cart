1674922c80f9d173697a930de4b059697ceb62a7
Crowe7 <jacobocrowe@gmail.com>
2022-05-20 17:28:17 -0500

    renamed files and added simple skeleton for local storage functions

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,12 @@
+import { productInterface } from "./productData";
+// CHECK IF LOGGED IN FIRST BEFORE SAVING ANYTHING... IF LOGGED IN DO NOT SAVE.
+export function saveCurrCart(cart: productInterface[]) {
+
+}
+
+// HAVE THIS BE SOMETHING THAT CAN WORK WHEN LOGGED IT IE WE CHECK IF ANYTHING IS IN LOCAL STORAGE AND IF SO PUT THAT INTO LOGGED IN USERS CART
+export function loadCurrCart() {
+    // maybe have this just return whats in local storage to use?
+}
+
+
